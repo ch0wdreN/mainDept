788895e28d1b2767305b79c70219d9ee6ac08b8f
i19yanagi <i19yanagi@gmail.com>
2022-09-26 18:07:46 +0900

    change: add navigate

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,9 +1,26 @@
 import type { Component } from 'solid-js'
+import { useNavigate } from '@solidjs/router'
+import { createSignal, JSX } from 'solid-js'
 
 const Top: Component = () => {
+  const navigate = useNavigate()
+  const [name, setName] = createSignal('default')
+  const onInputHandler: JSX.EventHandlerUnion<HTMLInputElement, Event> = (
+    e
+  ) => {
+    setName((e.target as HTMLInputElement).value)
+  }
   return (
     <>
-      <h3>名前を入力してください</h3>
+      <input
+        placeholder='名前を入力してください'
+        onInput={onInputHandler}
+        type='text'
+      />
+      <p>User Name: {name()}</p>
+      <button onClick={() => navigate('/game', { state: { name: name() } })}>
+        次へ
+      </button>
     </>
   )
 }
